35b8c40714b4d8a55f76e784c21186018607cc23
vdiaz <vhdiazro@gmail.com>
2020-07-11 21:09:04 -0500

    Primer commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+
+
+export const getGifs = async( category ) => {
+    
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=UDoeFAkdvCA1k88l34d7CYtEZQyFHhm3&q=${ encodeURI(category) }&limit=10`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }
+    })
+
+    return gifs;
+}
