0b62b60f9da6dff22b7d39751179d2e06687f7b8
vdiaz <vhdiazro@gmail.com>
2020-07-11 21:10:19 -0500

    docs agregado

---
+++ docs/precache-manifest.c352c82f61098bb6b92b2068e50f62a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4438480dbb683f8fcb459931619de691",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc18db8562e2440e5a9a",
+    "url": "/static/css/main.05401b6d.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc18db8562e2440e5a9a",
+    "url": "/static/js/main.85121667.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
